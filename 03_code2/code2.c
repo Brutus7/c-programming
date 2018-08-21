23c2ed8a588d8fbc3965044196f4a31d653bb834
Coursera Learner <nobody@nowhere.nul>
2018-08-21 13:09:39 +0000

    Did assignment 3

--- 03_code2/code2.c
+++ 03_code2/code2.c
@@ -32,7 +32,7 @@ int main(void) {
   numStars = printTriangle(7);
   //finally, print "That triangle had %d total stars\n", such that the %d 
   //prints the value of numStars
-  printf("That traingle had %d total stars\n", numStars);
+  printf("That triangle had %d total stars\n", numStars);
   return 0;
 }
 
